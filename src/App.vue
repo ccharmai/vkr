ac8d6e59c6c1c1335993e07569af7b46482ee3ce
Sergey Kozhevnikov <5429549@mail.ru>
2021-12-05 18:54:36 +0300

    Add button

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
-<template></template>
+<template>
+	<div id="app__wrapper">
+		<VButton placeholder="Кнопка" />
+		<VButton placeholder="Кнопка" disabled />
+	</div>
+</template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import VButton from './components/VButton.vue'
+
 export default defineComponent({
+	components: { VButton },
   setup() {},
 })
 </script>
+
+<style lang="scss">
+#app__wrapper {
+
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { join } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+	resolve: {
+		alias: [
+			{
+				// для алиасов в секции style
+				find: /~(.+)/,
+				replacement: join(__dirname, "src/$1"),
+			},
+
+			{
+				find: /@\//,
+				replacement: join(__dirname, "src") + "/",
+			},
+			{
+				find: "vue",
+				replacement: "vue/dist/vue.esm-bundler.js",
+			},
+		],
+	},
 })
