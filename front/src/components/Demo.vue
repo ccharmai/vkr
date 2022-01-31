0632884474ab90bef57d1d7bad8196dd9af9b230
Sergey Kozhevnikov <5429549@mail.ru>
2022-01-31 22:17:38 +0300

    Добавлена круговая диаграмма

--- front/src/components/Demo.vue
+++ front/src/components/Demo.vue
@@ -3,6 +3,7 @@
     <VInput v-model="line" placeholder="Имя" />
     <VInput v-model="line" placeholder="Имя" :disabled="isChecked" />
     <VCheck v-model="isChecked" text="Заблокировать ввод" />
+    <CircleProgress :percent="percent" />
   </div>
 </template>
 
@@ -11,15 +12,21 @@ import { defineComponent, ref } from 'vue'
 
 import VInput from '@/components/VInput.vue'
 import VCheck from '@/components/VCheck.vue'
+import CircleProgress from '@/components/graphs/CircleProgress.vue'
 
 export default defineComponent({
-  components: { VInput, VCheck },
+  components: { VInput, VCheck, CircleProgress },
   setup() {
     const line = ref('')
-
     const isChecked = ref(false)
 
-    return { line, isChecked }
+    const percent = ref(0)
+    setInterval(() => {
+      if (percent.value >= 100) percent.value = 0
+      else percent.value += 1
+    }, 200)
+
+    return { line, isChecked, percent }
   }
 })
 </script>
---
+++ front/src/components/graphs/CircleProgress.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="circle-progress">
+    <circle-progress
+      :percent="percent"
+      empty-color="#F7F7F8"
+      fill-color="#FF8F6B"
+      :border-width="10"
+      :border-bg-width="10"
+      :show-percent="true"
+      :size="100"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+// https://github.com/delowardev/vue3-circle-progress
+import "vue3-circle-progress/dist/circle-progress.css";
+// @ts-ignore
+import CircleProgress from "vue3-circle-progress"
+
+export default defineComponent({
+  components: { CircleProgress },
+  props: {
+    percent: { type: Number, required: true, validator: (value: number) => { return value >= 0 && value <= 100 } },
+  },
+  setup() {}
+})
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/style/vars';
+// .circle-progress {}
+</style>
