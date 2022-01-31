30e745adb1db493e8ba9afd51211fa98cf270200
Sergey Kozhevnikov <5429549@mail.ru>
2022-01-31 20:01:45 +0300

    Добавлен шрифт и заготовка под меню

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,3 +1,31 @@
 <template>
-  <div></div>
+  <div class="layout">
+    <TheMenu />
+    <div class="content"></div>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import TheMenu from '@/components/TheMenu.vue'
+
+export default defineComponent({
+  components: { TheMenu },
+  setup() {}
+})
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/style/vars';
+.layout {
+  width: 100%;
+  height: 100%;
+  padding: $p;
+  display: flex;
+  gap: $p;
+  .content {
+    flex-grow: 1;
+  }
+}
+</style>
---
+++ front/src/components/TheMenu.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="the-menu">
+    <div class="the-menu__container">
+      <div class="top">KK</div>
+      <div class="center">&</div>
+      <div class="bottom">$</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {}
+})
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/style/vars';
+.the-menu {
+  background-color: $white;
+  padding: $p;
+  border-radius: $br;
+  width: 90px;
+  &__container {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: $p;
+    align-items: center;
+    .top {}
+    .center {}
+    .bottom {
+      margin-top: auto;
+    }
+  }
+}
+</style>
